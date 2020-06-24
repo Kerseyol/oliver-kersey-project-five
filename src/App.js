0671fde9950e8f61a9969bff9d08665ae4880daf
Oliver Kersey <oliverjkersey@gmail.com>
2020-06-24 15:17:37 -0400

    most of the jsx done, trying for favicon

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ class App extends Component {
       <h2>Press a button below to see a piece of artwork from the Metropolitan Museum of Art featuring the corresponding animal!</h2>
       
       <div className="buttonAlignment">
-        <button value="cats" onClick={this.handleClick}>Cat</button>
-        <button value="dogs" onClick={this.handleClick}>Dog</button>
-        <button value="pigs" onClick={this.handleClick}>Pig</button>
-        <button value="dragons" onClick={this.handleClick}>Dragon</button>
+        <button value="tags:horse" onClick={this.handleClick}>Horses</button>
+        <button value="tags:dog" onClick={this.handleClick}>Dogs</button>
+        <button value="tags:bird" onClick={this.handleClick}>Birds</button>
+        <button value="tags:terms:dragon" onClick={this.handleClick}>Dragons</button>
       </div>
       
       {
--- src/ArtSearch.js
+++ src/ArtSearch.js
@@ -1,85 +1,81 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import App from './App';
 import './App.css';
-
-
 class ArtSearch extends Component {
-    
-    
-    constructor() {
-        super();
-        this.state = {
-            animalChoice: {},
-            artArray: [],
-            artResult: [],
-            finalArtwork: {},
-        }
-    }
-    
-    componentDidMount() {
-        let artResult;
-        axios({
-            url: 'https://collectionapi.metmuseum.org/public/collection/v1/search',
-            method: 'GET',
-            responseType: 'json',
-            params: {
-                q: this.props.userChoice,
-                hasImages:true,
-            }
-        }).then( (artArray) => {
-            artArray = artArray.data.objectIDs;
-            artResult = artArray[Math.floor(Math.random() * artArray.length)];
-            this.setState({
-                artArray,
-            })
-            
-//Then plug artResult into search for object, with accompanying image, info, etc
-//https://collectionapi.metmuseum.org/public/collection/v1/objects/ then artResult I guess
-            }).then( () => {
-
-                // console.log()
-                axios({
-                    url: `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artResult}`,
-                    method: 'GET',
-                    responseType: 'json',
-                    
-                }).then( (objectDetails) => {
-                    objectDetails = objectDetails.data;
-                    this.setState({
-                        objectDetails,
-                })
-                console.log("The second search happened")
-            })
-        }
-        ) 
+  constructor() {
+    super();
+    this.state = {
+      animalChoice: {},
+      artArray: [],
+      artResult: [],
+      finalArtwork: {},
+      objectDetails: [],
     };
-    
-    
-    render() {
-        // console.log(this.state.objectDetails && this.state.objectDetails.title)
-        return (
-            
-            <div>
-        <h1>{this.props.userChoice}</h1>
+  }
+  getApiData = () => {
+    let artResult;
+    axios({
+      url: 'https://collectionapi.metmuseum.org/public/collection/v1/search',
+      method: 'GET',
+      responseType: 'json',
+      params: {
+        q: this.props.userChoice,
+        hasImages: true,
+      },
+    })
+      .then((artArray) => {
+        artArray = artArray.data.objectIDs;
+        artResult = artArray[Math.floor(Math.random() * artArray.length)];
+        this.setState({
+          artArray,
+        });
+      })
+      .then(() => {
+        axios({
+          url: `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artResult}`,
+          method: 'GET',
+          responseType: 'json',
+        }).then((objectDetails) => {
+          objectDetails = objectDetails.data;
+          this.setState({
+            objectDetails,
+          });
+        });
+      });
+  };
+  componentDidMount() {
+    this.getApiData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.userChoice !== prevProps.userChoice) {
+      this.getApiData();
+    }
+  }
+
+  render() {
+    return (
+      <div className="artworkDisplay">
         <h2>{this.artResult}</h2>
         <h2>{this.state.objectDetails && this.state.objectDetails.title}</h2>
-        
-        {/* <p>{artResult}</p> */}
-    </div>
-            )
-            
-            
-            
-        };
-    }
-    
-    // <img src={this.state.artResult.primaryImage}></img>
-    
-    // <img src={this.state.finalArtwork.primaryImage} alt={this.state.finalArtwork.title}></img>
-    
-    export default ArtSearch;
+        <img src={this.state.objectDetails && this.state.objectDetails.primaryImage}></img>
+        {/* <a href='https://pngtree.com/so/decorative-border'>decorative-border png from pngtree.com</a> */}
+      </div>
+    );
+  }
+}
+// <img src={this.state.artResult.primaryImage}></img>
+// <img src={this.state.finalArtwork.primaryImage} alt={this.state.finalArtwork.title}></img>
+export default ArtSearch;
+// console.log(objectDetails)
+// }).then( (response) => {
+//     console.log(response);
+//     this.setState({
+//         finalArtwork: response.data
+//     })
     // console.log(objectDetails)
+    // <img src={this.state.finalArtwork.primaryImage} alt={this.state.finalArtwork.title}></img>
+    // <img src={this.state.artResult.primaryImage}></img>
 // }).then( (response) => {
 //     console.log(response);
 //     this.setState({
