6db8ccafa72f963e531ab2e4ab80e0084e5a2dfb
Oliver Kersey <oliverjkersey@gmail.com>
2020-06-23 20:25:52 -0400

    try3 the tryening

--- src/ArtSearch.js
+++ src/ArtSearch.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import ArtResult from './ArtResult'
 import App from './App';
 import './App.css';
 
@@ -29,25 +28,29 @@ class ArtSearch extends Component {
             }
         }).then( (artArray) => {
             artArray = artArray.data.objectIDs;
-            const artResult = artArray[Math.floor(Math.random() * artArray.length)];
+            this.artResult = artArray[Math.floor(Math.random() * artArray.length)];
             this.setState({
                 artArray,
             })
-            console.log(artResult)
+            console.log("The first search happened")
+            console.log(this.artResult)
+            console.log(`Now we've just got to get ${this.artResult} into the search URL`)
             
 //Then plug artResult into search for object, with accompanying image, info, etc
 //https://collectionapi.metmuseum.org/public/collection/v1/objects/ then artResult I guess
             }).then(
             axios({
-                url: 'https://collectionapi.metmuseum.org/public/collection/v1/objects/',
+                url: 'https://collectionapi.metmuseum.org/public/collection/v1/objects',
                 method: 'GET',
                 responseType: 'json',
+                
             }).then( (objectDetails) => {
                 objectDetails = objectDetails.data;
                 this.setState({
                     objectDetails,
                 })
-                console.log(objectDetails)
+                console.log("The second search happened")
+                // console.log(objectDetails)
             }).then( (response) => {
                 console.log(response);
                 this.setState({
@@ -58,15 +61,14 @@ class ArtSearch extends Component {
     };
     
     
-    
     render() {
         
         return (
             
             <div>
-        <h3>{this.props.userChoice}</h3>
+        <h1>{this.props.userChoice}</h1>
         
-        {/* <p>{artArray}</p> */}
+        {/* <p>{artResult}</p> */}
     </div>
             )
             
@@ -79,11 +81,4 @@ class ArtSearch extends Component {
         
     // <img src={this.state.finalArtwork.primaryImage} alt={this.state.finalArtwork.title}></img>
     
-
-
-
-
-
-
-
 export default ArtSearch;
